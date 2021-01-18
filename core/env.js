18e35319e26e6f00fadc46448e86adf943a2aef0
tk-ni <talkalepanagos@gmail.com>
2021-01-18 22:09:36 +0200

    db update

--- core/env.js
+++ core/env.js
@@ -2,11 +2,11 @@ module.exports = {
     PORT: process.env.PORT || 3201,
     database: {
         credentials: {
-            host: 'localhost', //process.env.DB_HOST
-            user: 'root',  //process.env.DB_USER
-            password: '', //process.env.DB_PASS
-            port: '3306', //process.env.DB_PORT
-            database: 'puppeteer_map_db', //process.env.DB_NAME,
+            host: process.env.DB_HOST, //process.env.DB_HOST
+            user: process.env.DB_USER,  //process.env.DB_USER
+            password: process.env.DB_PASS, //process.env.DB_PASS
+            port: process.env.DB_PORT, //process.env.DB_PORT
+            database: process.env.DB_NAME, //process.env.DB_NAME,
             insecureAuth: true
         },
         queueTableName: 'queue',
