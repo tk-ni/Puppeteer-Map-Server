be9b8c103ef531e544064c7559b8661e89b58e43
tk-ni <talkalepanagos@gmail.com>
2021-01-18 21:00:58 +0200

    cors fix

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, {
-    cors: {
-        origin: '*',
-    }
-});
+const io = require('socket.io')(http);
 
 const env = require('./core/env');
 const crawler = require('./crawler/crawler');
